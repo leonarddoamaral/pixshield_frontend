7ee2dceddf85d5dfa7865ce7fbbaf3744fa5e98a
Kleber Tadeu <klebertadeu17@gmail.com>
2025-11-15 22:36:13 -0300

    Tela de Login criada
    A tela de login foi criada, necessitando apenas de polimento no css

--- pix-shield/src/pages/main.tsx
+++ pix-shield/src/pages/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Cadastro from './Cadastro'
 import './main.css'
+import Login from './Login'
+import Cadastro from './Cadastro'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Cadastro />
+    <Login />
   </StrictMode>,
 )
