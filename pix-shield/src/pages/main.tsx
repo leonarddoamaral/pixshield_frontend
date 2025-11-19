9bf54ac6036f8034342347ccd1ae8010b2a7e1ab
Kleber Tadeu <102772826+K-Tadeu@users.noreply.github.com>
2025-11-19 10:10:55 -0300

    Set up routing with RouterProvider in main.tsx

--- pix-shield/src/pages/main.tsx
+++ pix-shield/src/pages/main.tsx
@@ -1,11 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Home from './Home'
 import './main.css'
-import Cadastro from './Cadastro'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Documentacao from './Documentacao'
 import Login from './Login'
+import Cadastro from './Cadastro'
+import PerfilUser from './Perfil'
+import Registro from './Registro/Registro'
+import Sucesso from './confimaDenuncia/Denuncia'
+
+const router = createBrowserRouter([
+  {path: "/", element: <Home />},
+  {path: "/login", element: <Login />},
+  {path: "/cadastro", element: <Cadastro />},
+  {path: "/documentacao", element: <Documentacao />},
+  {path: "/home", element: <Home />},
+  {path: "/perfil", element: <PerfilUser />},
+  {path: "/registro", element: <Registro/>},
+  {path: "/sucesso", element:<Sucesso/>}
+  // {path: "/perfil", element: }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Cadastro />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
